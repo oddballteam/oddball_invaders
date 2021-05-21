7fec7d716bf1aa6f13793190dc0db358c478c35e
Lee DeBoom <lee.deboom@oddball.io>
2021-05-21 15:15:09 -0500

    more enemies, dark background, background music

--- app/main.rb
+++ app/main.rb
@@ -3,6 +3,13 @@ require 'app/enemy.rb'
 require 'app/score_keeper.rb'
 
 def tick(args)
+  args.state.colors.background ||= [10, 10, 20]
+  args.state.colors.text ||= [255, 255, 255]
+  args.state.colors.star ||= [128, 200, 255]
+  args.state.night ||= [args.grid.rect, args.state.colors.background]  
+  args.outputs.solids << args.state.night
+  args.outputs.sounds << 'sounds/background.wav' if args.state.tick_count == 0
+
   args.state.score_keeper ||= ScoreKeeper.new(args)
   args.state.score_keeper.display_score
 
@@ -11,7 +18,7 @@ def tick(args)
   args.state.player.handle_shoot
 
   args.state.enemies ||= []
-  args.state.enemies.push(Enemy.new(args)) if (args.state.tick_count % (rand(40) + 150)) == 0
+  args.state.enemies.push(Enemy.new(args)) if (args.state.tick_count % (rand(10) + 10)) == 0
   temp_enemies = []
   args.state.enemies.each do |enemy|
     enemy.handle_move
